e1073094d5c4b07dc9d17d19e0ca9c1804be792b
idanordstrom99 <idanordstrom99@gmail.com>
2025-11-12 16:33:15 +0100

    I ADDED "HEJ"

--- run_visunet.R
+++ run_visunet.R
@@ -7,3 +7,5 @@ load_all("~/Documents/Forskningspraktik/VisuNet")
 ros <- rosetta(autcon)
 # VisuNet with GO annotations
 vis <- visunet(ros$main, addGO = TRUE, GO_ontology = "MF", NodeSize = "sum")
+
+#HEJHEJ
